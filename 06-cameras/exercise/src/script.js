891c599bc00cc238735af30b55512ce298b7e794
Jonathan Isaacs-Heyliger <heyligerjon@gmail.com>
2024-11-18 11:18:04 -0500

    semicolons

--- 06-cameras/exercise/src/script.js
+++ 06-cameras/exercise/src/script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 /**
@@ -7,43 +7,43 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const cursor = {
     x: 0,
     y: 0
-}
+};
 window.addEventListener('mousemove', (event) => {
     cursor.x = event.clientX / sizes.width - 0.5;
     cursor.y = - (event.clientY / sizes.height - 0.5);
-})
+});
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Sizes
 const sizes = {
     width: 800,
     height: 600
-}
+};
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Object
 const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(mesh)
+);
+scene.add(mesh);
 
 // Camera
-const aspectRatio = sizes.width / sizes.height
-const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100)
-// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
-// camera.position.x = 3
-// camera.position.y = 2
-camera.position.z = 3
-camera.lookAt(mesh.position)
-scene.add(camera)
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100);
+// camera.position.x = 3;
+// camera.position.y = 2;
+camera.position.z = 3;
+camera.lookAt(mesh.position);
+scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
@@ -52,15 +52,15 @@ controls.enableDamping = true;
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
+});
+renderer.setSize(sizes.width, sizes.height);
 
 // Animate
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime();
 
     // Update objects
     // mesh.rotation.y = elapsedTime;
@@ -75,10 +75,10 @@ const tick = () =>
     controls.update();
 
     // Render
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 
     // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+    window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
