c0634d8dc8492e22fc10461ea9c91a95e9b325df
Jonathan Isaacs-Heyliger <heyligerjon@gmail.com>
2024-12-22 20:42:56 -0500

    finished lesson 17

--- 17-particles/exercise/src/script.js
+++ 17-particles/exercise/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Timer } from 'three/addons/misc/Timer.js';
 import GUI from 'lil-gui';
 
 /**
@@ -17,7 +18,42 @@ const scene = new THREE.Scene();
 /**
  * Textures
  */
+// Geometry
 const textureLoader = new THREE.TextureLoader();
+const particlesTexture = textureLoader.load('/textures/particles/4.png');
+
+// Material
+const particlesGeometry = new THREE.BufferGeometry();
+
+const count = 20000;
+const positionsArray = new Float32Array(count * 3);
+const colorsArray = new Float32Array(count * 3);
+
+for(let i = 0; i < count * 3; i++) {
+    positionsArray[i] = (Math.random() - 0.5) * 10;
+    colorsArray[i] = Math.random();
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positionsArray, 3));
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colorsArray, 3));
+
+const particlesMaterial = new THREE.PointsMaterial({
+    // color: 0xff88cc,
+    size: 0.1,
+    sizeAttenuation: true,
+    map: particlesTexture,
+    transparent: true,
+    alphaMap: particlesTexture,
+    // alphaTest: 0.001
+    // depthTest: false,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true
+});
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 /**
  * Test cube
@@ -26,7 +62,7 @@ const cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial()
 );
-scene.add(cube);
+// scene.add(cube);
 
 /**
  * Sizes
@@ -75,11 +111,23 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 /**
  * Animate
  */
-const clock = new THREE.Clock();
+const timer = new Timer();
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime();
+    timer.update();
+    const elapsedTime = timer.getElapsed();
+
+    // Update particles
+    // particles.position.y = -elapsedTime / 10;
+
+    // Bad practice, the ideal way to animate is using a custom shader
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3;
+        const x = particlesGeometry.attributes.position.array[i3];
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x);
+        particlesGeometry.attributes.position.needsUpdate = true;
+    }
 
     // Update controls
     controls.update();
